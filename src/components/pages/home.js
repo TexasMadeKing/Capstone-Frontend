90f03ecb6227c364908970b517ce4d5f9cc2bf0f
Josh Bennetsen <65803112+TexasMadeKing@users.noreply.github.com>
2023-08-30 22:32:02 -0500

    Update home.js

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,70 +1,42 @@
-// This page will render a Log in button if your not logged in, upon clicking will lead you to a login page.
-// If you're logged in, it will render a Log out button with your email address.
+// This page will render a "Log in" button if you're not logged in; upon clicking, it will lead you to a login page.
+// If you're logged in, it will render a "Log out" button with your email address.
 
-import React from "react"
-// import { useNavigate } from "react-router-dom";
-// import Login from "../../components/auth/login";
-// import "..//..//components/"
-import styled from 'styled-components';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+// Removed unused import: import Login from "../../components/auth/login";
 
-const StyledButton = styled.button`
-    padding: 12px 24px;
-    margin: 8px;
-    background-color: cornflowerblue;
-    color: white;
-    border: none;
-    border-radius: 8px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 40px;
-`;
-
-const StyledInput = styled.input`
-    padding: 10px;
-    margin: 10px;
-    width: 80%;
-    transition: background-color 0.7s ease;
-`;
-
-const StyledDiv = styled.div`
-    padding: 12px 24px;
-    margin: 8px;
-    font-style: 40px
-`;
-
-function App() {
-    return <StyledButton>Click Me</StyledButton>;
-}
+import("../styles/home.scss")
 
 
 
-const Home = (props) => {   // passing data from one api to another
-    const { loggedIn, email } = props
-    // const navigate = useNavigate();
+const Home = (props) => { // TODO: Describe how data is passed from one API to another
+    const { loggedIn, email } = props;
+    // eslint-disable-next-line
+    const navigate = useNavigate();
     
     const onButtonClick = () => {
-        // I'll update this function later
+        // TODO: Update this function later
     };
 
-    
-        return(
-            <StyledDiv>
-            <div className="mainContainer">
-            <div className={"titleContainer"}>
-                <div>Welcom!</div>
+    return (
+        <div className="mainContainer">
+            <div className="titleContainer">
+                <div>Welcome Back!</div>
             </div>
-            This is the home page
-
-            <StyledButton type="submit">Login</StyledButton>
             <div>
-
+                This is the home page.
             </div>
+            <div className="buttonContainer">
+                <input
+                    className="inputButton"
+                    type="button"
+                    onClick={onButtonClick}
+                    value={loggedIn ? "Log out" : "Log in"} 
+                />
+                {loggedIn ? <div>Your email address is {email}</div> : null}
             </div>
-            </StyledDiv>
-        );
-    }
-
+        </div>
+    );
+};
 
 export default Home;
