6995ad0ba368606a43942a6dddc96917109df417
Marcedes <118416606+MarSpen89@users.noreply.github.com>
2023-08-30 22:56:39 -0400

    styled

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,42 +2,69 @@
 // If you're logged in, it will render a Log out button with your email address.
 
 import React from "react"
-import { useNavigate } from "react-router-dom";
-import Login from "../../components/auth/login";
-import "../styles"; //Import the SCSS file
+// import { useNavigate } from "react-router-dom";
+// import Login from "../../components/auth/login";
+// import "..//..//components/"
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    padding: 12px 24px;
+    margin: 8px;
+    background-color: cornflowerblue;
+    color: white;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+`;
+
+const StyledInput = styled.input`
+    padding: 10px;
+    margin: 10px;
+    width: 80%;
+    transition: background-color 0.7s ease;
+`;
+
+const StyledDiv = styled.div`
+    padding: 12px 24px;
+    margin: 8px;
+    font-style: 40px
+`;
+
+function App() {
+    return <StyledButton>Click Me</StyledButton>;
+}
+
+
 
 const Home = (props) => {   // passing data from one api to another
     const { loggedIn, email } = props
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     
     const onButtonClick = () => {
         // I'll update this function later
+    };
+
+    
+        return(
+            <StyledDiv>
+            <div className="mainContainer">
+            <div className={"titleContainer"}>
+                <div>Welcom!</div>
+            </div>
+            This is the home page
+
+            <StyledButton type="submit">Login</StyledButton>
+            <div>
+
+            </div>
+            </div>
+            </StyledDiv>
+        );
     }
 
 
-
-    return <div className="mainContainer">
-        <div className={"titleContainer"}>
-            <div>Welcome Back!</div>
-        </div>
-        <div>
-
-            This is the home page.
-            
-        </div>
-        <div className={"buttonContainer"}>
-            <input
-                className={"inputButton"}
-                type="button"
-                onClick={onButtonClick}
-                value={loggedIn ? "Log out" : "Log in"} />
-            {(loggedIn ? <div>
-                Your email address is {email}
-            </div> : <div/>)}
-        </div>
-
-
-    </div>
-}
-
-export default Home
+export default Home;
